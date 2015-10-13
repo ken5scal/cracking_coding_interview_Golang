3d3eefaa07a3adc729c24f007bb555b2662ad031
ken5scal <kengoscal@gmail.com>
2015-10-14 02:03:05 +0900

    Add next, prev, first method

--- doubly_linked_list.go
+++ doubly_linked_list.go
@@ -9,3 +9,19 @@ type Node struct {
 	Person
 	next, prev *Node
 }
+
+type List struct {
+	head, tail *Node
+}
+
+func (l *List) First() *Node {
+	return l.head
+}
+
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+func (n *Node) Prev() *Node {
+	return n.prev
+}
